4c0465e7e42c3810fd6f6ab2a99358b7d4f391c8
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-17 16:52:07 +0800

    list&index.py

--- Day 3/leapyear.py
+++ Day 3/leapyear.py
@@ -7,14 +7,12 @@ def find_leap_years(given_year):
     count = 0
     
     while count < 15:
-        if given_year % 4 == 0 or given_year % 400 == 0:
-            if given_year % 100 != 0:
-                list_of_leap_years.append(given_year)
-                count += 1
-            given_year += 1
-        
-     
+        if given_year % 4 == 0 and (given_year % 400 == 0 or given_year % 100 != 0):
+          list_of_leap_years.append(given_year)
+          count += 1
+        given_year += 1
+    
     return list_of_leap_years
 
-list_of_leap_years=find_leap_years(2000)
+list_of_leap_years=find_leap_years(1900)
 print(list_of_leap_years)
