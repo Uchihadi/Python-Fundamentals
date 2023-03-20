8ed69335ce8f20664e0092eac7f014b213f42a09
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-20 11:30:04 +0800

    Update listlvl2ex.py

--- Day 3/listlvl2ex.py
+++ Day 3/listlvl2ex.py
@@ -7,8 +7,8 @@ def get_count(num_list):
     # num_list
     for num in range(len(num_list) - 1):
         if num_list[num] == num_list[num + 1]:
-            count += 1
-        return count
+            count =count + 1
+    return count
 
 #provide different values in list and test your program
 num_list=[1,1,5,100,-20,-20,6,0,0]
