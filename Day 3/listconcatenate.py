d717ecff7b519f356ba98ea3a08c98d5f4de068b
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-20 09:37:37 +0800

    iteration structures

--- Day 3/listconcatenate.py
+++ Day 3/listconcatenate.py
@@ -5,11 +5,11 @@ def create_largest_number(number_list):
     #remove pass and write your logic here
     number_list.sort() #sorting in ascending order
     number_list.reverse()
-    numbers = "" #Initialise the numbers
+    numbers = " " #Initialise the numbers
     
     for i in number_list:
         numbers += str(i) #str i concatenates to the left
-    return int(numbers)
+    return numbers
     
 number_list=[23,45,67]
 largest_number=create_largest_number(number_list)
