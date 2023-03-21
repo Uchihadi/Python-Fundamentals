a9f84e5bb965bd9a06f481f39b804feb652b7d67
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-21 12:19:31 +0800

    update day 4

---
+++ Day 4/findcorrectdict.py
@@ -0,0 +1,8 @@
+#lex_auth_01269444890062848087
+
+def find_correct(word_dict):
+    pass
+    #start writing your code here
+
+word_dict={"THEIR": "THEIR","BUSINESS":"BISINESS","WINDOWS":"WINDMILL","WERE":"WEAR","SAMPLE":"SAMPLE"}
+print(find_correct(word_dict))
