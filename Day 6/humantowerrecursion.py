7c6a1ffb66e4d8c9772facc6ccf8c49366802397
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-23 12:21:39 +0800

    Create humantowerrecursion.py

---
+++ Day 6/humantowerrecursion.py
@@ -0,0 +1,13 @@
+#lex_auth_01269437527007232044
+
+def human_pyramid(no_of_people):
+    pass #remove pass and place the recursive code the you had written earlier for this function
+
+def find_maximum_people(max_weight):
+    no_of_people=0
+    #write your logic here. You may invoke recursive function human_pyramid() wherever applicable 
+    return no_of_people
+
+#Provide different values for max_weight and test your program
+max_people=find_maximum_people(1000)
+print(max_people)
