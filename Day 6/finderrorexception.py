e1e5643746dc7b48da1bcbc9ff27ff797cc78ca8
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-23 13:04:13 +0800

    day 6

--- Day 6/finderrorexception.py
+++ Day 6/finderrorexception.py
@@ -2,10 +2,22 @@
 
 def find_average(mark_list):
 	total=0
-	for i in range(0, len(mark_list)):
-		total+=mark_list[i]
-	marks_avg=total/len(mark_list)
-	return marks_avg
+ 
+	try:
+		for i in range(0, len(mark_list)):
+			total+=mark_list[i]
+		marks_avg=total/len(mark_list)
+		return marks_avg
+
+	except NameError:
+     	 print("Name error Occured")
+       
+	except ValueError:
+     	 print("Value error Occured")
+       
+	except TypeError:
+		print ("Type Error Occurred")
+     
 
 m_list=[1,2,3,4]
 print("Average marks:", find_average(m_list))
--- Day 6/humantowerrecursion.py
+++ Day 6/humantowerrecursion.py
@@ -1,12 +1,24 @@
 #lex_auth_01269437527007232044
 
 def human_pyramid(no_of_people):
-    pass #remove pass and place the recursive code the you had written earlier for this function
+    #remove pass and place the recursive code the you had written earlier for this function
+    if (no_of_people == 1):
+        return 1*50
+    else:
+        return no_of_people*50 + human_pyramid(no_of_people - 2)
 
 def find_maximum_people(max_weight):
-    no_of_people=0
-    #write your logic here. You may invoke recursive function human_pyramid() wherever applicable 
-    return no_of_people
+    #write your logic here. You may invoke recursive function human_pyramid() wherever applicable
+    no_of_people = 1
+    while True:
+        weight = human_pyramid (no_of_people)
+        
+        if weight <= max_weight:
+            no_of_people +=2
+        else:
+            break
+ 
+    return no_of_people - 2
 
 #Provide different values for max_weight and test your program
 max_people=find_maximum_people(1000)
