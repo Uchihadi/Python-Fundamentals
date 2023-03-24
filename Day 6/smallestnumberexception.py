af99dc4e50d6fc3dce84f85bf212c9037a014969
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-24 10:38:29 +0800

    Update smallestnumberexception.py

--- Day 6/smallestnumberexception.py
+++ Day 6/smallestnumberexception.py
@@ -1,10 +1,19 @@
 #lex_auth_01269442760027340879
 
-def find_smallest_number(num):
-    pass
-    #start writing your code here
+def find_divisors(num):
+    divisors = [] #Initialising an empty list for divisors
+    for i in range (1, num + 1): #It is a start (1) and end value @ num
+        if num % i == 0:
+            divisors.append(i)
+    return len(divisors) #Returns the length of the list
 
-num=16
-print("The number of divisors :",num)
-result=find_smallest_number(num)
-print("The smallest number having",num," divisors:",result)
+def find_smallest_number(num):
+    #start writing your code here
+    for i in range (2, 1000): #Running a for-loop from start to end
+        if find_divisors(i) == num: #Runs another function to find list of divisors
+            return i
+
+num = 16
+print ("The number of divisors :", num)
+result = find_smallest_number (num)
+print ("The smallest number having", num," divisors:", result)
