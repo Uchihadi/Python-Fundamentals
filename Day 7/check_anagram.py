0b9e98c23ae185f5b0daf2b8bc1676b66d0f0a85
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-26 22:31:11 +0800

    Update check_anagram.py

--- Day 7/check_anagram.py
+++ Day 7/check_anagram.py
@@ -4,9 +4,9 @@ def check_anagram(data1,data2):
     lst = []
     for i in data1:
         for j in data2:
-            if i == j:
-                return True
-    
+            if len(i) == len(j):
+                if i == j:
+                    return True
     #start writing your code here
 
-print(check_anagram("eat","tea"))
+print(check_anagram("about","table"))
