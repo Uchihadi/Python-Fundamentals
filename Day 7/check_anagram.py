ab7b7d5cbff7be19d6afb94af85581f0cadd03b7
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-03-26 22:45:16 +0800

    Update check_anagram.py

--- Day 7/check_anagram.py
+++ Day 7/check_anagram.py
@@ -1,6 +1,25 @@
 #lex_auth_0127382206342184961397
 
+# Accepts two strings and return True if one string is a special anagram of another string
+# Special Annagram: 1) Contain Repeating Characters 
+# 2) None of characters repeat at same position
+# 3) Length of the strings should be the same
+
 def check_anagram(data1,data2):
+    first = data1.lower()
+    second = data2.lower()
+    
+    d1 = []
+    d2 = []
+
+    for i in range(0, len(first)):
+        d1.append(first[i])
+    for i in range(0, len(second)):
+        d2.append(second[i])
+
+    print (d1)
+    print (d2)
+    
     lst = []
     for i in data1:
         for j in data2:
