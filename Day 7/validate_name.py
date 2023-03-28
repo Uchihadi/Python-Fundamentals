438a626f393d853af23fe5964179a6af217f9034
Uchihadi <adilovescode@gmail.com>
2023-03-29 00:07:17 +0800

    Create validate_name.py

---
+++ Day 7/validate_name.py
@@ -0,0 +1,27 @@
+#lex_auth_012694465248329728100
+
+def validate_name(name):
+    pass
+    #Start writing your code here
+
+def validate_phone_no(phno):
+    pass
+    #Start writing your code here
+
+def validate_email_id(email_id):
+    pass
+    #Start writing your code here
+
+def validate_all(name,phone_no,email_id):
+    pass
+    #Start writing your code here
+    # Use the below given print statements to display appropriate messages
+    # Also, do not modify them for verification to work
+    #print("Invalid Name")
+    #print("Invalid phone number")
+    #print("Invalid email id")
+    #print("All the details are valid")
+
+
+#Provide different values for name, phone_no and email_id and test your program
+validate_all("Tina", "9994599998", "tina@yahoo.com")
