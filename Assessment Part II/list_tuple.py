0b64d39b84aff8ccfa16c164c20cd9510798314d
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-04-04 10:51:45 +0800

    Update list_tuple.py

--- Assessment Part II/list_tuple.py
+++ Assessment Part II/list_tuple.py
@@ -2,6 +2,7 @@ try:
     tupl1 = ([1,2], [3, 4])
     list1 = [(1, 2), (3, 4)]
     list2 = tupl1 [0] #Will Overwrite List 2 but not the Tuple
+    list2 [0] = 5
     list1 [1] = (6,7)
     print(tupl1, list1)
 except TypeError:
